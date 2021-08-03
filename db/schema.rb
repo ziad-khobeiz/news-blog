7aa70bb9c9ab0165b61749cd54a68297a041aed1
Ziad Khobeiz <ziadkhobeiz@hotmail.com>
2021-08-03 20:00:09 +0200

    Add Journalists' Accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_174221) do
+ActiveRecord::Schema.define(version: 2021_08_03_175245) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 2021_08_03_174221) do
     t.boolean "approved"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "journalists", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_journalists_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_journalists_on_reset_password_token", unique: true
   end
 
 end
