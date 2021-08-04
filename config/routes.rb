66f93710daa18314ccf4083afed1d322fdb1cebb
Ziad Khobeiz <ziadkhobeiz@hotmail.com>
2021-08-04 18:59:48 +0200

    Remove route for edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'articles/approve'
   get 'home/index'
   devise_for :journalists
-  resources :articles
+  resources :articles, :except => [:edit, :update]
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
