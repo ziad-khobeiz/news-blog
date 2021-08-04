684f70433375f62f478deb7e0d029ed28d09887d
Ziad Khobeiz <ziadkhobeiz@hotmail.com>
2021-08-04 14:31:45 +0200

    Validate name presence

--- app/models/journalist.rb
+++ app/models/journalist.rb
@@ -4,4 +4,5 @@ class Journalist < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles
+  validates :first_name, :last_name, presence: true
 end
