b6b58021d22f8f8fd44843fa95dce8e0a9aa0c80
Ziad Khobeiz <ziadkhobeiz@hotmail.com>
2021-08-04 23:24:48 +0200

    Sort published articles by most popular authors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.where(approved: true)
+    results = ActiveRecord::Base.connection.execute(
+    "select journalist_id from Articles
+    where Articles.approved = true
+    group by journalist_id
+    order by count(*) DESC")
+    @articles = Array.new
+    results.each do |result|
+        @articles.concat(Article.where(approved: true, journalist_id: result["journalist_id"]).to_a)
+    end
   end
 end
