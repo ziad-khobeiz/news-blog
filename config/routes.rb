4f1339a17e1327cdd095564a5448d3a3a4f2238f
Ziad Khobeiz <ziadkhobeiz@hotmail.com>
2021-08-05 12:36:17 +0200

    Remove the route for passwords and devise shared links for the login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'articles/approval_requests'
   get 'articles/approve/:id', to: 'articles#approve', as: :approve
   get 'home/index'
-  devise_for :journalists
+  devise_for :journalists, :skip => [:passwords]
   resources :articles, :except => [:edit, :update]
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
